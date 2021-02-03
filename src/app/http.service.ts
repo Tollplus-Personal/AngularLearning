8af8f74d6409a8a567f7c3a3f2be18edd0495dd8
Nikhil-Dev-Hack <75464384+Nikhil-Dev-Hack@users.noreply.github.com>
2021-02-03 17:56:11 +0530

    commit3

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HttpService {
+  constructor() {}
 
-  constructor() { }
+  myMethod() {
+    return console.log("Hey, What is up!");
+  }
 }
